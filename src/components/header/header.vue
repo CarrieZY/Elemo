87cc3515068ce872e178246c28d1dea16d357b42
tenzo <2275182981@qq.com>
2018-10-18 23:10:12 +0800

    header

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -17,12 +17,12 @@
                     <span>{{seller.supports[0].description}}</span>
                 </div>
             </div> 
-            <div v-if="seller.supports" class="supports-count">
+            <div v-if="seller.supports" class="supports-count" @click="Showdateil">
                 <span class="count">{{seller.supports.length+'个'}}</span>
                 <i class="icon-keyboard_arrow_right"></i>
             </div>
         </div>
-        <div class="bulletin-warpper">
+        <div class="bulletin-warpper" @click="Showdateil">
             <span class="bulletin-title"></span>
             <span class="bulletin-text">{{seller.bulletin}}</span>
             <i class="icon-keyboard_arrow_right"></i>
@@ -30,10 +30,44 @@
         <div class="bg">
             <img :src="seller.avatar" width="100%" height="100%">
         </div>
+        <div v-show="detailShow" class="detail">
+            <div class="detail-warpper clearfix">
+                <div class="detail-main">
+                    <p class="name">{{seller.name}}</p>
+                    <!-- 星星组件 -->
+                    <div class="star-warpper">
+                        <star :size='48' :score="seller.score"></star>
+                    </div>
+                    <div class="title">
+                        <div class="line"></div>
+                        <div class="text">优惠信息</div>
+                        <div class="line"></div>
+                    </div>
+                    <ul v-if="seller.supports" class="supports">
+                        <li class="support-item" v-for="(item,index) in seller.supports" key="index">
+                        <span class="icon" :class="classMap[seller.supports[index].type]"></span>
+                        <span class="text">{{seller.supports[index].description}}</span>
+                        </li>
+                    </ul>
+                    <div class="title">
+                        <div class="line"></div>
+                        <div class="text">商家公告</div>
+                        <div class="line"></div>
+                    </div>
+                    <div class="bulletin">
+                        <p class="content">{{seller.bulletin}}</p>
+                    </div>
+                </div>
+            </div>
+            <div class="detail-close" @click="hideDetail">
+                <i class="icon-close"></i>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import star from 'components/star/star'
 export default {
     name:'HeaderTop',
     props:{
@@ -41,8 +75,24 @@ export default {
             type:Object
         }
     },
-    created(){
+    data(){
+        return {
+            detailShow:false
+        }
+    },
+    methods:{
+        Showdateil (){
+            this.detailShow = true
+        },
+        hideDetail (){
+            this.detailShow = false
+        }
+    },
+    created (){
         this.classMap=['decrease','discount','guarantee','invoice','special']
+    },
+    components:{
+        star
     }
 }
 </script>
@@ -155,5 +205,89 @@ export default {
             position:absolute
             top:0
             left:0
+        .detail
+            position fixed
+            z-index 100
+            width:100%
+            height:100%
+            overflow auto 
+            background rgba(7,17,27,.8)
+            top:0
+            left 0
+            .detail-warpper
+                min-height 100%
+                width 100%
+                .detail-main
+                    margin-top 64px
+                    padding-bottom 60px
+                    .name
+                        font-size:16px
+                        line-height:16px 
+                        color:#fff
+                        font-weight 700
+                        text-align center
+                    .star-warpper
+                        margin-top:18px 
+                        padding 2px 0 
+                        text-align center 
+                    .title
+                        width 80%
+                        margin 30px auto 24px auto 
+                        display flex
+                        .line
+                            flex 1
+                            position relative
+                            top:-6px
+                            border-bottom:1px solid rgba(255,255,255,.2)
+                        .text
+                            padding 0 12px 
+                            font-size:14px
+                            font-weight 700
+                    .supports
+                        width:80%
+                        margin:0 auto 
+                        .support-item
+                            padding 0 12px
+                            margin-bottom 12px
+                            font-size:0
+                            &:last-child
+                                margin-bottom 0
+                            .icon
+                                display inline-block
+                                height 16px
+                                width:16px
+                                background-repeat no-replace
+                                margin-right:6px 
+                                background-size 16px 16px
+                                &.decrease
+                                    bg-image('decrease_2')
+                                &.discount
+                                    bg-image('discount_2')
+                                &.guarantee
+                                    bg-image('guarantee_2')   
+                                &.invoice
+                                    bg-image('invoice_2')
+                                &.special
+                                    bg-image('special_2')  
+                            .text
+                                line-height:12px
+                                font-size:12px 
+                    .bulletin
+                        width:80%
+                        margin:0 auto 
+                        .content
+                            padding 0 12px 
+                            line-height:24px 
+                            font-size 12px
+
+
+            .detail-close
+                position relative
+                width:32px
+                height:32px
+                margin:-60px auto 0 auto 
+                clear both
+                font-size:32px
+
 
 </style>
